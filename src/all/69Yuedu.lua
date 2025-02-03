97222cd2572fef6eae639ff6e7632a4e4bcc6572
AdAeternum <57020092+InInfinitumEtInTenebris@users.noreply.github.com>
2025-02-03 11:12:51 -0600

    Create 69Yuedu.lua

---
+++ src/all/69Yuedu.lua
@@ -0,0 +1,7 @@
+-- {"id":5271,"ver":"1.0.0","libVer":"1.0.0","author":"unknownazzwipe","dep":["Madara>=2.2.0"]}
+
+return Require("Madara")("https://69yuedu.net/", {
+    id = 5271,
+    name = "69Yuedu",
+    imageURL = "https://69yuedu.net/",
+})
