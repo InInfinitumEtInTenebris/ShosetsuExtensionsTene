01b90b8acf39ce3ced584bb78784c62ce10571a6
AdAeternum <57020092+InInfinitumEtInTenebris@users.noreply.github.com>
2025-09-16 18:29:24 -0500

    Update FanMTL.lua

--- src/all/FanMTL.lua
+++ src/all/FanMTL.lua
@@ -1,4 +1,4 @@
--- {"id":1308639964,"ver":"1.1.5","libVer":"1.0.0","author":"Jobobby04","dep":["ReadWN>=1.0.11"]}
+-- {"id":1308639964,"ver":"1.1.5","libVer":"1.0.0","author":"unknownazzwipe","dep":["ReadWN>=1.0.11"]}
 
 local GENRES = {
     "All",
@@ -62,7 +62,7 @@ local GENRES = {
 }
 
 return Require("ReadWN")("https://www.fanmtl.com", {
-    id = 1308639964,
+    id = 1308639968,
     name = "FanMTL",
     imageURL = "https://github.com/jobobby04/ShosetsuExtensions/raw/master/icons/wuxia_box.png",
     shrinkURLNovel = "^.-fanmtl%.com",
